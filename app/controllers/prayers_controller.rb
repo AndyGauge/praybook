bd536111c18d975ca084e706ebc95a99ad279dd1
Andrew Gauger <andygauge@gmail.com>
2015-10-29 15:45:05 -0700

    prayers beginning
    introducing prayers controller show and index

--- app/controllers/prayers_controller.rb
+++ app/controllers/prayers_controller.rb
@@ -1,2 +1,14 @@
 class PrayersController < ApplicationController
+	before_action :select_prayer_from_id, only: [:show]
+	def index
+		@prayers = Prayer.all
+	end
+
+	def show
+	end
+
+	def select_prayer_from_id
+		@prayer = Prayer.find(params[:id])
+	end
+
 end
