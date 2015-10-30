551ded734d6b34ee3224fd1cfb32e72d1b2f02eb
Andrew Gauger <andygauge@gmail.com>
2015-10-30 13:31:21 -0700

    s s s ss

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   #resources :people  #TODO: create people without accounts via refferences
   resources :prayers, :only => [:index, :show]
   resources :praises, :only => [:index, :show]
-  root 'user#new'
+  root 'users#new'
   #resources :posts  #POSTS are not publicly consumed, use children
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
