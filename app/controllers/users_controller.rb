59fbf8164b09eced5904c87eaec856e1d59955a2
Andrew Gauger <andygauge@gmail.com>
2015-10-30 12:46:01 -0700

    user controller generally
    a simple user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+
+	before_action :select_user_from_id, only: [:show, :edit]
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to :action => 'show'
+		else
+			render :action => 'edit'
+		end
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def select_user_from_id
+		@user = User.find(param[:id])
+	end
+
+	def user_params
+		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	end
+
 end
