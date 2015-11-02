371c918668ceb9d775122a3e130f5d77b7b740d2
Andrew Gauger <andygauge@gmail.com>
2015-11-02 11:31:07 -0800

    adding pretty names

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,11 @@
 class Person < ActiveRecord::Base
 	#attr_accessor :name
 	attr_reader  :email
+	before_save :capitalize_name
 
 	#TODO: add texting capibility
 	#validates :name, length: {minimum: 4}
+	def capitalize_name
+		name = name.titleize
+	end
 end
