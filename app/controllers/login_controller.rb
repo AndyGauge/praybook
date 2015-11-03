756de70545788a9e7a473ef7b503242723e056db
Andrew Gauger <andygauge@gmail.com>
2015-11-03 12:15:51 -0800

    taking a stab at login

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,2 +1,18 @@
 class LoginController < ApplicationController
+	def new 
+		#@user = guest? ? User.new : current_user
+	end
+	def create
+		user = User.find_by(params[:login][:email])
+		if user && user.authenticate(params[:login][:password])
+			session[:current_user_id] = user.id
+			#render the logged on page
+		else
+			redirect_to login_url, alert: "Failed to logon"
+		end
+	end
+	def destroy
+		slay_current_user
+		redirect_to root_url
+	end
 end
