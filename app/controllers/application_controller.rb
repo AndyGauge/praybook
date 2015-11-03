e44425bc26b86d9b9b5891b6110b93bc569ee9cc
Andrew Gauger <andygauge@gmail.com>
2015-11-03 11:31:28 -0800

    add the session into the mix.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  def current_user
+  	@current_user ||= session[:current_user_id] ? Person.find(session[:current_user_id]) : new_guest_user
+  end
+
+  def new_guest_user
+  	Person.find(session[:current_user_id] = Person.create({}).id)
+  end
+  def guest?
+  	# In single table inheritance it would appear that the type is nil for Person
+  	!!current_user.type
+  end
+
 end
