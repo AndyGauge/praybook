d0f7615d954d576cadd7108c1d405fe49b5bb308
Andrew Gauger <andygauge@gmail.com>
2015-11-03 16:20:22 -0800

    people be person is

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -2,19 +2,19 @@ require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
 	test "guest is valid person" do 
-		assert person(:guest).valid?
+		assert people(:guest).valid?
 	end
 	test "person with name capitalizes" do
-		p = person(:person_with_name)
+		p = people(:person_with_name)
 		p.save
 		assert_equal p.name, p.name.to_s.titleize
 	end
 	test "person with capital name remains" do
-		p = person(:person_with_capital_name)
+		p = people(:person_with_capital_name)
 		assert p.save
 		assert_equal p.name, p.name.to_s.titleize
 	end
 	test "person with email has email" do
-		assert_not_nil person(:person_with_email).email
+		assert_not_nil people(:person_with_email).email
 	end
 end
