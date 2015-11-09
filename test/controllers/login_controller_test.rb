0e786918dd9ab7cdd19b942e4586f8a9ebaa9a69
Andrew Gauger <andygauge@gmail.com>
2015-11-09 15:05:25 -0800

    login tests

--- test/controllers/login_controller_test.rb
+++ test/controllers/login_controller_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class LoginControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "should display login form"
+		get :new
+		assert_response :success
+	end
+
+	test "should fail login with bad attempt"
+		post :create, {email: "lrose@gmail.com", password: "fail"}
+		assert_not_empty flash[:alert]
+		assert_redirect_to login_url
+	end
+
+	test "should login successfully"
+		post :create, {email: "bob@email.com", password: "password"}
+		assert_empty flash[:alert]
+		assert_not_nil session[:current_user_id]
+		assert_response :success
 end
