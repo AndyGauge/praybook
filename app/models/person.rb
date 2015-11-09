55acd4e5974d03a6b2ec1bd6a52896aadcb2ec67
Andrew Gauger <andygauge@gmail.com>
2015-11-09 11:51:10 -0800

    tidy up models

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,10 +1,7 @@
 class Person < ActiveRecord::Base
-	#attr_accessor :name
-	#attr_reader  :email
 	before_save :capitalize_name
 
 	#TODO: add texting capibility
-	#validates :name, length: {minimum: 4}
 	private
 	def capitalize_name
 		self.name = self.name.to_s.titleize
