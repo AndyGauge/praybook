6cbe88a16cb253962569e8ee6aeaa045cf8c8689
Andrew Gauger <andygauge@gmail.com>
2015-11-19 16:19:13 -0800

    a few new user tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			redirect_to root_url
+			redirect_to user_url
 		else
 			render :action => 'edit'
 		end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "can get new user form" do
+		get :new
+		assert_response :success
+	end
+
+	test "can submit user blank" do
+		post :create, user: {}
+		assert_redirected_to user_path
+	end
+
+	test "should create new user" do
+		assert_difference('User.count') do
+			post :create, user: { 
+				name: "UserControllerTest:test_user", 
+				email: "test_user@UserControllerTest", 
+				password: "passw0RD", 
+				password_confirmation: "passw0RD" 
+			}
+		end
+		assert_redirect_to user_url
+	end
+		
 end
