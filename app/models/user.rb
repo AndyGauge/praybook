b04c7a20baed35341e1f47094c201906de2b170d
Andrew Gauger <andygauge@gmail.com>
2015-11-20 12:13:22 -0800

    first nav stab

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < Person
+	# Users require all fields be filled out, passwords match,
+	# and email is unique (used for authentication)
+	validates :name, presence: true
+	validates :email, uniqueness: true
 	has_secure_password
 end
