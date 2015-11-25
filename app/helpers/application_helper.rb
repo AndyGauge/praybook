f483e52259adc3f8f9ce017b8c9ec99c006225a7
Andrew Gauger <andygauge@gmail.com>
2015-11-25 14:48:28 -0800

    closing paranthesis

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper 
   def pb_menu_item (controller_name, path: nil, display: controller_name.capitalize)
     path ||= send("#{controller_name.downcase}_path") 
-    "<li class=\"nav-item #{' active' if current_page?(path}\">
+    "<li class=\"nav-item #{' active' if current_page?(path)}\">
        <a class=\"nav-link pb-navbar-link\"href=\"#{path}\">#{display}</a>
      </li>"
   end
