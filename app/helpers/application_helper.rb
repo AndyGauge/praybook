c8d831237d845b38a964b2d9274eb88f3bfc109c
Andrew Gauger <andygauge@gmail.com>
2015-11-25 14:47:07 -0800

    try try again

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper 
-  def pb_menu_item (controller_name, path: "#{controller_name.downcase}_path", display: controller_name.capitalize)
-    "<li class=\"nav-item #{' active' if current_page?(controller: controller_name)}\">
-       <a class=\"nav-link pb-navbar-link\"href=\"#{send(path)}\">#{display}</a>
+  def pb_menu_item (controller_name, path: nil, display: controller_name.capitalize)
+    path ||= send("#{controller_name.downcase}_path") 
+    "<li class=\"nav-item #{' active' if current_page?(path}\">
+       <a class=\"nav-link pb-navbar-link\"href=\"#{path}\">#{display}</a>
      </li>"
   end
 end
