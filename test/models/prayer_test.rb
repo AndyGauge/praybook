2a1fc59d9072b4a13de9c6e5c4048fb331fdcd4e
Andrew Gauger <andygauge@gmail.com>
2015-12-07 11:40:25 -0800

    still failing

--- test/models/prayer_test.rb
+++ test/models/prayer_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class PrayerTest < ActiveSupport::TestCase
 
 	test "should respond to complete!" do
-		posts(:second_prayer).complete!
-		assert posts(:second_prayer).is_a?(Praise)
+		@p = posts(:second_prayer).complete!
+		assert @p.type = "Praise"
 	end
 end
