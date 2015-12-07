0a030a3788fc7cd57b888ed27adcd73a70df8304
Andrew Gauger <andygauge@gmail.com>
2015-12-07 11:41:35 -0800

    again

--- test/models/prayer_test.rb
+++ test/models/prayer_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class PrayerTest < ActiveSupport::TestCase
 
 	test "should respond to complete!" do
-		@p = posts(:second_prayer).complete!
+		@p = posts(:second_prayer)
+		@p.complete!
 		assert @p.type = "Praise"
 	end
 end
