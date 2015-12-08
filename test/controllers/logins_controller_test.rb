cd719c14787d3d48b2a01dd32dd7f9a0d57166c5
Andrew Gauger <andygauge@gmail.com>
2015-12-08 12:57:06 -0800

    fix login controller for form_for symbol, navbar-classes

--- test/controllers/logins_controller_test.rb
+++ test/controllers/logins_controller_test.rb
@@ -9,20 +9,20 @@ class LoginsControllerTest < ActionController::TestCase
 	end
 
 	test "should fail login with bad attempt" do
-		post :create, {email: "lrose@gmail.com", password: "fail"}
+		post :create, login: {email: "lrose@gmail.com", password: "fail"}
 		assert_not_empty flash[:alert]
 		assert_redirected_to login_path
 	end
 
 	test "should login successfully" do
-		post :create, {email: "bob@email.com", password: "password"}
+		post :create, login: {email: "bob@email.com", password: "password"}
 		assert_nil flash[:alert]
 		assert_not_nil session[:current_user_id]
 		assert_redirected_to prayers_path
 	end
 
 	test "should log out after log in successful and destroyed" do
-		post :create, {email: "bob@email.com", password: "password"}
+		post :create, login: {email: "bob@email.com", password: "password"}
 		assert_nil flash[:alert]
 		post :destroy
 		assert_nil session[:current_user_id]
