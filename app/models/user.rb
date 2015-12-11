13eafaf5dae94be49a059c51d5c63232deb0d8e4
Andrew Gauger <andygauge@gmail.com>
2015-12-11 13:00:54 -0800

    habtm self join

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < Person
 	validates :name, presence: true
 	validates :email, uniqueness: true
 	has_secure_password
+
+ 	has_and_belongs_to_many :friends,
+      class_name: "User", 
+      foreign_key: :user_id, 
+      association_foreign_key: :friend_id
 end
