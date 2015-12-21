9a3598016bc94752df19e25b4bba604060f606b4
Andrew Gauger <andygauge@gmail.com>
2015-12-21 15:15:57 -0800

    friendly people

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < Person
-	# Users require all fields be filled out, passwords match,
+	# Users require all fields be filled out, 
 	# and email is unique (used for authentication)
 	validates :name, presence: true
 	validates :email, uniqueness: true
 	has_secure_password
 
  	has_and_belongs_to_many :friends,
-      class_name: "User", 
+      class_name: "Person", 
       join_table: :friends_users, 
       foreign_key: :user_id, 
       association_foreign_key: :friend_id
