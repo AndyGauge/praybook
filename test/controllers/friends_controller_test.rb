2baac163e14fcd87570fad9235fb81c0650a2256
Andrew Gauger <andygauge@gmail.com>
2015-12-28 10:58:07 -0800

    more friends tests

--- test/controllers/friends_controller_test.rb
+++ test/controllers/friends_controller_test.rb
@@ -11,13 +11,23 @@ class FriendsControllerTest < ActionController::TestCase
   		name: "bob",
   		email: "bobby@bobsdomain.bob"
   	}, current_user_id: people(:user_who_can_update).id
+  	assert_redirected_to friends_path
+  end
+
+  test "should add existing new friend" do
+  	post :create, person: {
+  		name: "bob",
+  		email: "lrose@gmail.com"
+  	}, current_user_id: people(:user_who_can_update).id
+  	assert_redirected_to friends_path
   end
 
   test "should destroy friendship" do
   	@user_id = people(:user_who_can_update).id
   	assert_difference("User.find(#{@user_id}).friends.count", -1) do
-	  	delete :destroy, id: people(:person_with_name).id,
-	  	  current_user_id: @user_id
+	  	delete( :destroy, {'id' => people(:person_with_name).id},
+		  	  {'current_user_id' => @user_id})
 	  end
+	  assert_redirected_to friends_path
 	end
 end
