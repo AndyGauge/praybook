e49364ad51e50ffaca2a8951a1f80c6e180941f8
Andrew Gauger <andygauge@gmail.com>
2015-12-29 13:25:11 -0800

    depreciation warning

--- test/controllers/prayers_controller_test.rb
+++ test/controllers/prayers_controller_test.rb
@@ -34,13 +34,13 @@ class PrayersControllerTest < ActionController::TestCase
 
 	test "should find Prayer and prayer_for on one page" do
 		Prayer.per_page=30  # Default
-		get(:show, {id: 1}, {'current_user_id' => people(:person_with_name)})
+		get(:show, {id: 1}, {'current_user_id' => people(:person_with_name).id})
 		assert_response :success
 	end
 
 	test "should find Prayer_for after prayers" do
 		Prayer.per_page=2
-		get(:show, {id: 2}, {'current_user_id' => people(:person_with_name)})
+		get(:show, {id: 2}, {'current_user_id' => people(:person_with_name).id})
 		assert_response :success
 	end
 
